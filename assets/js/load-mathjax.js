08f23bd52b6298b80ef13453a7922acbd98ee878
thomaswiemann <wiemann@uchicago.edu>
2024-11-12 19:11:35 -0600

    change footer, add scores

--- assets/js/load-mathjax.js
+++ assets/js/load-mathjax.js
@@ -1,6 +1,7 @@
 window.MathJax = {
   tex: {
-    inlineMath: [['$', '$']]
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']]
   },
   svg: {
     fontCache: 'global'
@@ -9,7 +10,7 @@ window.MathJax = {
 
 (function () {
   var script = document.createElement('script');
-  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+  script.src = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.0/es5/tex-mml-chtml.js"; 
   script.async = true;
   document.head.appendChild(script);
 })();
